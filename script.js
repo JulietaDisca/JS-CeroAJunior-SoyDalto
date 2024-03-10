836ec2059b89b174d3ea92393d1acaa990f71c70
juli.disca <discajulieta@gmail.com>
2024-03-10 16:11:48 -0300

    Capitulo 1 - Dia 2

---
+++ script.js
@@ -0,0 +1,19 @@
+/*recipiente = "barbijo";
+
+alert();
+/*String = "cadena de texto"
+number = 19
+booleano = false */
+
+/*Formas de declarar una variable*/
+/*var, let, const;*//*let: menos alcance - var: alcance global(todo el archivo)*/
+
+/*var nombre; */
+
+/*let nombre = null;
+
+alert(nombre);*/
+
+let nombre = prompt ("dime tu nombre");
+
+alert(nombre + " me quiere mucho");
